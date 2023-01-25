856f6bd419cbafb5aeb36980085eeaf478a24678
timzi <timzik06@gmail.com>
2023-01-25 22:58:38 +0300

    предзащитные рофланыV1.01

--- main.py
+++ main.py
@@ -28,15 +28,15 @@ class Citilink:
         if name is not None:
             name = (str(name).split('\n'))[-2]
         else:
-            name = 'Сайт магазина не отвечает.'
+            name = 'No Data'
         return name.strip()
 
     def get_img(self):
-        img = str(self.data.find('img'))
-        if not img:
+        img = (self.data.find('img'))
+        if img is not None:
             img = str(self.data.find('img')['src'])
         else:
-            img = ''
+            img = 'static/placeholder.png'
         return img
 
 
@@ -67,32 +67,46 @@ class tm24(Citilink):
         price = self.data.find('div', class_='price-values').text
         return price.strip()
 
+    def get_name(self):
+        name = str(self.data.find('h1', itemprop='name').text)
+        return name
+
 
 app = Flask(__name__)
+sleep(120)
 Ctl1 = Citilink('lap1')
+sleep(30)
 Tml1 = AppleWave('lap1')
 Ctl2 = Citilink('lap2')
+sleep(30)
 Tml2 = AppleWave('lap2')
 Ctl3 = Citilink('lap3')
-Tml3 = AppleWave('lap3')
 sleep(30)
+Tml3 = AppleWave('lap3')
+sleep(120)
 
 Ctp1 = Citilink('phone1')
+sleep(30)
 AWp1 = AppleWave('phone1')
 Tmp1 = tm24('phone1')
 Ctp2 = Citilink('phone2')
+sleep(30)
 AWp2 = AppleWave('phone2')
 Tmp2 = tm24('phone1')
 Ctp3 = Citilink('phone3')
+sleep(30)
 AWp3 = AppleWave('phone3')
 Tmp3 = tm24('phone1')
-sleep(30)
+sleep(120)
 
 Ctt1 = Citilink('TV1')
+sleep(30)
 Tmt1 = tm24('TV1')
 Ctt2 = Citilink('TV2')
+sleep(30)
 Tmt2 = tm24('TV2')
 Ctt3 = Citilink('TV3')
+sleep(30)
 Tmt3 = tm24('TV3')
 
 flag = 0
@@ -105,15 +119,22 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/info')
+def info():
+    global flag
+    flag = 0
+    return render_template('info.html')
+
+
 @app.route('/laptops')
 def laptops():
     global flag
     flag = 1
-    name_one = Ctl1.get_name()
+    name_one = Tml1.get_name()
     url_one = str(Ctl1.get_img())
-    name_two = Ctl2.get_name()
+    name_two = Tml2.get_name()
     url_two = str(Ctl2.get_img())
-    name_three = Ctl3.get_name()
+    name_three = Tml3.get_name()
     url_three = str(Ctl3.get_img())
     return render_template('template.html',
                            name_one=name_one,
@@ -149,11 +170,11 @@ def phones():
 def TVs():
     global flag
     flag = 3
-    name_one = Ctt1.get_name()
+    name_one = Tmt1.get_name()
     url_one = str(Ctt1.get_img())
-    name_two = Ctt2.get_name()
+    name_two = Tmt2.get_name()
     url_two = str(Ctt2.get_img())
-    name_three = Ctt2.get_name()
+    name_three = Tmt2.get_name()
     url_three = str(Ctt2.get_img())
     return render_template('template.html',
                            name_one=name_one,
@@ -168,22 +189,26 @@ def TVs():
 @app.route('/good_one')
 def good1():
     if flag == 1:
-        name = Ctl1.get_name()
+        name = Tml1.get_name()
+        image = Ctl1.get_img()
         shop_one = 'citilink.ru ' + Ctl1.get_price() + ' Р'
         shop_two = 'telemarket24.ru ' + Tml1.get_price() + ' Р'
         shop_three = ''
     elif flag == 2:
         name = AWp1.get_name()
+        image = Ctp1.get_img()
         shop_one = 'citilink.ru ' + Ctp1.get_price() + ' Р'
         shop_two = 'applewave.ru ' + AWp1.get_price() + ' Р'
         shop_three = 'telemarket24.ru ' + str(Tmp1.get_price())
     else:
-        name = Ctt1.get_name()
+        name = Tmt1.get_name()
+        image = Ctt1.get_img()
         shop_one = 'citilink.ru ' + Ctt1.get_price() + ' Р'
         shop_two = 'telemarket24.ru ' + Tmt1.get_price() + ' Р'
         shop_three = ''
     return render_template('good.html',
                            name=name,
+                           image=image,
                            shop_one=shop_one,
                            shop_two=shop_two,
                            shop_three=shop_three)
@@ -192,21 +217,25 @@ def good1():
 @app.route('/good_two')
 def good2():
     if flag == 1:
-        name = Ctl2.get_name()
+        name = Tml2.get_name()
+        image = Ctl2.get_img()
         shop_one = 'citilink.ru ' + Ctl2.get_price() + ' Р'
         shop_two = 'telemarket24.ru ' + Tml2.get_price() + ' Р'
         shop_three = ''
     elif flag == 2:
         name = AWp2.get_name()
+        image = Ctp2.get_img()
         shop_one = 'citilink.ru ' + Ctp2.get_price() + ' Р'
         shop_two = 'applewave.ru ' + AWp2.get_price() + ' Р'
         shop_three = 'telemarket24.ru ' + str(Tmp2.get_price())
     else:
-        name = Ctt2.get_name()
+        name = Tmt2.get_name()
+        image = Ctt2.get_img()
         shop_one = 'citilink.ru ' + Ctt2.get_price() + ' Р'
         shop_two = 'telemarket24.ru ' + Tmt2.get_price() + ' Р'
         shop_three = ''
     return render_template('good.html',
+                           image=image,
                            name=name,
                            shop_one=shop_one,
                            shop_two=shop_two,
@@ -216,22 +245,26 @@ def good2():
 @app.route('/good_three')
 def good3():
     if flag == 1:
-        name = Ctl3.get_name()
+        name = Tml3.get_name()
+        image = Ctl3.get_img()
         shop_one = 'citilink.ru ' + Ctl3.get_price() + ' Р'
         shop_two = 'telemarket24.ru ' + Tml3.get_price() + ' Р'
         shop_three = ''
     elif flag == 2:
         name = AWp3.get_name()
+        image = Ctp3.get_img()
         shop_one = 'citilink.ru ' + Ctp3.get_price() + ' Р'
         shop_two = 'applewave.ru ' + AWp3.get_price() + ' Р'
         shop_three = 'telemarket24.ru ' + str(Tmp3.get_price())
     else:
-        name = Ctt3.get_name()
+        name = Tmt3.get_name()
+        image = Ctt3.get_img()
         shop_one = 'citilink.ru ' + Ctt3.get_price() + ' Р'
         shop_two = 'telemarket24.ru ' + Tmt3.get_price() + ' Р'
         shop_three = ''
     return render_template('good.html',
                            name=name,
+                           image=image,
                            shop_one=shop_one,
                            shop_two=shop_two,
                            shop_three=shop_three)
